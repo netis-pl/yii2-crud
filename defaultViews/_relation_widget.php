d2edd08f497a209231571c8874b208c588b0d000
Patryk Radziszewski <pradziszewski@netis.pl>
2015-05-27 16:21:17 +0200

    add collapse to detail relations grid

--- defaultViews/_relation_widget.php
+++ defaultViews/_relation_widget.php
@@ -15,13 +15,22 @@ $relation = $relations[$relationName];
 /** @var ActiveRecord $model */
 $model = $relation['model'];
 ?>
-
-<section>
-    <h1><?= $model->getCrudLabel('relation') ?></h1>
-    <?php Pjax::begin(); ?>
-    <?= GridView::widget([
-        'dataProvider' => $relation['dataProvider'],
-        'columns'      => $relation['columns'],
-    ]); ?>
-    <?php Pjax::end(); ?>
-</section>
+<div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="heading<?= $relationName ?>">
+        <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse<?= $relationName ?>" aria-expanded="true" aria-controls="collapse<?= $relationName ?>">
+                <?= $model->getCrudLabel('relation'); ?>
+            </a>
+        </h4>
+    </div>
+    <div id="collapse<?= $relationName ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading<?= $relationName ?>">
+        <div class="panel-body">
+            <?php Pjax::begin(); ?>
+            <?= GridView::widget([
+                'dataProvider' => $relation['dataProvider'],
+                'columns'      => $relation['columns'],
+            ]); ?>
+            <?php Pjax::end(); ?>           
+        </div>
+    </div>
+</div>    
