6f86a69c088fc5cc791901c3d7d8afa2d9c344fb
Patryk Radziszewski <pradziszewski@netis.pl>
2015-05-27 09:26:48 +0200

    reations on detail view

--- defaultViews/view.php
+++ defaultViews/view.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\grid\GridView;
+use yii\data\ActiveDataProvider;
 
 /* @var $this yii\web\View */
 /* @var $model yii\db\ActiveRecord */
@@ -22,4 +24,20 @@ $this->params['menu'] = $controller->getMenu($controller->action, $model);
     'model' => $model,
     'attributes' => $attributes,
 ]) ?>
+<!-- just for now -->
+<?php foreach ($relations as $relation): ?>
+    <section>
+        <h1>Nazwa</h1>
+<?php
+$dataProvider = new ActiveDataProvider(['query' => $relation])
+?>
 
+        <?=
+        GridView::widget([
+            'dataProvider' => $dataProvider,
+            //'filterModel' => $searchModel,
+//            'columns'      => $columns,
+        ]);
+        ?>
+    </section>
+<?php endforeach; ?>
