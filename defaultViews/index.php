b8869e93985808b2277155b4945c6156944ee59e
Bernard Chaniewicz <bchaniewicz@netis.pl>
2015-05-28 11:55:57 +0200

    quick search

--- defaultViews/index.php
+++ defaultViews/index.php
@@ -17,11 +17,30 @@ $this->params['menu'] = $controller->getMenu($controller->action, $searchModel);
 ?>
 
 <h1><span><?= Html::encode($this->title) ?></span></h1>
-
+<?php
+$script = <<<JS
+jQuery('#w0').yiiGridView({'filterUrl':'\/assortment\/product','filterSelector':'#form-control'});
+JS;
+//$this->registerJs($script,3);
+?>
+<?php
+$script = <<<JS
+jQuery('body').on('keyup','.form-control',function(){
+    $("#w0").yiiGridView("applyFilter");})
+JS;
+//$this->registerJs($script,3);
+?>
 <?= netis\utils\web\Alerts::widget() ?>
-
+<div class="input-group" style="width: 200px;">
+    <span class="input-group-addon"><i class="fa fa-search"></i></span>
+    <!--<input onkeyup="jQuery('#w0').addClass('asd');"-->
+    <div id="w0-filters">
+    <input onkeyup="jQuery('#w0').yiiGridView('applyFilter')"
+        class="form-control" id="form-control" name="Product[Symbol]" placeholder="<?php echo Yii::t('app', 'Search'); ?>" type="text"/>
+    </div>
+</div>
 <?= GridView::widget([
     'dataProvider' => $dataProvider,
-    //'filterModel' => $searchModel,
+    'filterModel' => $searchModel,
     'columns' => $columns,
 ]); ?>
