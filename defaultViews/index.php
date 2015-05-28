b72d3b99d52887d466f6d8b0796bfd92891e4cbe
Bernard Chaniewicz <bchaniewicz@netis.pl>
2015-05-28 16:58:05 +0200

    quick search

--- defaultViews/index.php
+++ defaultViews/index.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\widgets\Pjax;
+
 
 /* @var $this yii\web\View */
 /* @var $searchModel netis\utils\db\ActiveSearchTrait */
@@ -17,30 +19,21 @@ $this->params['menu'] = $controller->getMenu($controller->action, $searchModel);
 ?>
 
 <h1><span><?= Html::encode($this->title) ?></span></h1>
-<?php
-$script = <<<JS
-jQuery('#w0').yiiGridView({'filterUrl':'\/assortment\/product','filterSelector':'#form-control'});
-JS;
-//$this->registerJs($script,3);
-?>
-<?php
-$script = <<<JS
-jQuery('body').on('keyup','.form-control',function(){
-    $("#w0").yiiGridView("applyFilter");})
-JS;
-//$this->registerJs($script,3);
-?>
 <?= netis\utils\web\Alerts::widget() ?>
+<?php Pjax::begin(); ?>
 <div class="input-group" style="width: 200px;">
-    <span class="input-group-addon"><i class="fa fa-search"></i></span>
-    <!--<input onkeyup="jQuery('#w0').addClass('asd');"-->
-    <div id="w0-filters">
-    <input onkeyup="jQuery('#w0').yiiGridView('applyFilter')"
-        class="form-control" id="form-control" name="Product[Symbol]" placeholder="<?php echo Yii::t('app', 'Search'); ?>" type="text"/>
-    </div>
+    <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
+    <form data-pjax>
+        <div id="w0-filters">
+            <input class="form-control" id="form-control" name="search" placeholder="<?php echo Yii::t('app', 'Search'); ?>" type="text"/>
+        </div>
+    </form>
 </div>
+
 <?= GridView::widget([
     'dataProvider' => $dataProvider,
-    'filterModel' => $searchModel,
+//    'filterModel' => $searchModel,
+    'filterSelector' => '#form-control',
     'columns' => $columns,
 ]); ?>
+<?php Pjax::end(); ?>
