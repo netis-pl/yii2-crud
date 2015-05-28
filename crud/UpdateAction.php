2814d7116f65cea5d96959fdf8fff2a490496207
Jan Was <jwas@nets.com.pl>
2015-05-28 14:25:19 +0200

    fix checkbox styling in update form

--- crud/UpdateAction.php
+++ crud/UpdateAction.php
@@ -183,10 +183,6 @@ class UpdateAction extends Action
         switch ($formats[$attribute]) {
             case 'boolean':
                 $formFields[$attribute]['formMethod'] = 'checkbox';
-                $formFields[$attribute]['options'] = [
-                    'template' => '{beginLabel}{input}<span>{labelTitle}</span>{help}{error}{endLabel}',
-                    'class' => 'checkbox style-2',
-                ];
                 break;
             case 'time':
                 $formFields[$attribute]['formMethod'] = 'textInput';
