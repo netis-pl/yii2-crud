38ada7c61beee289de7d35e477137fbfb6d89bf9
Mateusz <mgryc@netis.pl>
2015-09-18 13:04:23 +0200

    implements PhpManager

---
+++ rbac/IAuthManager.php
@@ -0,0 +1,20 @@
+<?php
+namespace netis\utils\rbac;
+
+interface IAuthManager
+{
+    /**
+     * Returns a list of auth items between the one checked and the one assigned to the user,
+     * after a successful checkAccess() call.
+     * @return array
+     */
+    public function getCurrentPath();
+
+    /**
+     * This method is only used in \netis\utils\web\User.can() when loading cached results.
+     * @param array $path
+     * @return $this
+     */
+    public function setCurrentPath($path);
+
+}
